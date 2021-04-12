be876134203344e7de730e70cf996d25108d67ac
YuliyaTurovskaya <turovskaya.yuliya24@gmail.com>
2021-04-12 16:44:28 +0300

    feat: add solution task encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,25 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const array = str.split('');
+  const result = [];
+  let count = 1;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === array[i + 1]) {
+      count += 1;
+    } else if (array[i] !== array[i + 1]) {
+      if (count === 0 || count === 1) {
+        result.push(array[i]);
+      } else {
+        result.push(count, array[i]);
+      }
+      count = 1;
+    }
+  }
+
+  return result.join('');
 }
 
 module.exports = encodeLine;
